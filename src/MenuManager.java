46d07c71103aa3120536856d134811c85afea952
lg <lg@DESKTOP-OK384K0>
2023-03-19 15:16:21 +0900

    removed helloWorld.java and added menumanger.java

---
+++ src/MenuManager.java
@@ -0,0 +1,11 @@
+
+public class MenuManager {
+	
+	public static void main(String[] args) {
+		System.out.println("1. Add stocks");
+		System.out.println("2. Delete stocks");
+		System.out.println("3. Edit stocks");
+		System.out.println("4. View stocks");
+		System.out.println("5. Exit");
+	}
+}
