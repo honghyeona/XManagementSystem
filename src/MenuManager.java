316e0201d33a65054dcecdf552b2cbcdeab36c83
lg <lg@DESKTOP-OK384K0>
2023-04-13 15:36:17 +0900

    added MenuManager

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,38 +1,71 @@
+
 import java.util.Scanner;
 
 public class MenuManager {
-	
+
 	public static void main(String[] args) {
-		int num = 0;
-		Scanner input = new Scanner(System.in);
+		Scanner input =new Scanner(System.in);
+		StocksManager stocksManager=new StocksManager(input);
 		
-		while(num!=6) {
-		System.out.println("1. Add stocks");
-		System.out.println("2. Delete stocks");
-		System.out.println("3. Edit stocks");
-		System.out.println("4. View stocks");
-		System.out.println("5. Show a menu");
-		System.out.println("6. Exit");
-		System.out.println("Select one number between 1-6:");
-		num = input.nextInt();
+		int num  =-1;
+		while(num!=5) {
+			System.out.println("***Stocks Management System Menu***");
+			System.out.println("1. Add stocks");
+			System.out.println("2. Delete stocks");
+			System.out.println("3. Edit stocks");
+			System.out.println("4. View stocks");
+			System.out.println("5. Exit");
+			System.out.println("Select one number between 1-5:");
+			num=input.nextInt();
+			if (num==1) {
+				stocksManager.addStocks();
+			}
+			else if (num==2) {
+				stocksManager.deleteStocks();
+			}
+			else if (num==3) {
+				stocksManager.editStocks();
+			}
+			else if (num==4) {
+				stocksManager.viewStocks();
+			}
+			else {
+				continue;
+			}
+			
+		}
+	}
+	public static void addStocks() {
+		Scanner input =new Scanner(System.in);	
 		
-		switch(num) {
-		case 1:
-			System.out.print("Stock Id:");
-			int stockId = input.nextInt();
-			System.out.print("Stock Item:");
-			String stockItem = input.next();
-			System.out.print("Stock Name:");
-			String stockName = input.next();
-			break;
-		case 2:
-		case 3:
-		case 4:
-			System.out.print("Stock Id:");
-			int stockId2 = input.nextInt();
-	
+		System.out.print("Stocks ID: "); 
+		int stocksID=input.nextInt();
+		
+		System.out.print("Stocks Item: ");
+		String stocksItem=input.next();
+		System.out.println(stocksItem);
+
+		System.out.print("stocksName:");
+		String stocksName=input.next();
+		System.out.println(stocksName);
 	}
 	
-  }
-}
+	
+	public static void deleteStocks() {
+		Scanner input =new Scanner(System.in);
+		System.out.print("Stocks ID: ");
+		int stocksID = input.nextInt();
+	}
+	public static void editStocks() {
+		Scanner input =new Scanner(System.in);
+		System.out.print("Stocks ID: ");
+		int stocksID = input.nextInt();
+	}
+	
+	public static void viewStocks() {
+		Scanner input =new Scanner(System.in);
+		System.out.print("Stocks ID: ");
+		int stocksID = input.nextInt();
+	}
+	
 }
