b29ad5e7e49a86819b3d81b84eafb6516bd1278b
lg <lg@DESKTOP-OK384K0>
2023-05-01 14:56:36 +0900

    added to MartStock

---
+++ src/stock/MartStock.java
@@ -0,0 +1,36 @@
+package stock;
+
+import java.util.Scanner;
+
+public class MartStock extends Stock {
+	
+	public void getUserInput(Scanner input) {
+		System.out.print("Stock ID: "); 
+	    int id=input.nextInt();
+	    this.setId(id);
+	    
+	    char answer='x';
+	    while(answer!='y'&&answer!='Y'&&answer!='n'&&answer!='N') {
+	      System.out.print("Do you have an item? (Y/N)");
+	      answer=input.next().charAt(0);
+	      if(answer=='y'||answer=='Y') {
+	    	  System.out.print("Stock Item: ");
+			  String item=input.next();
+			  this.setItem(item);
+			  break;
+	      }
+	      else if(answer=='n'||answer=='N') {
+	    	  this.setItem("");
+	    	  break;
+	      }
+	      else {
+	      }
+	    }
+		
+		System.out.print("stock Name: ");
+		String name=input.next();
+		this.setName(name);
+	}
+
+
+}
