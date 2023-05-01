71c12d2103acee4db50bcbc807d757903469dc66
lg <lg@DESKTOP-OK384K0>
2023-05-01 14:57:04 +0900

    added to Stock

--- src/stock/Stock.java
+++ src/stock/Stock.java
@@ -1,9 +1,44 @@
+package stock;
+
+import java.util.Scanner;
 
 public class Stock {
+	protected StockKind kind=StockKind.ConvenienceStore;
+	protected int id;
+	protected String item;
+	protected String name;
 	
-	int id;
-	String item;
-	String name;
+	public StockKind getKind() {
+		return kind;
+	}
+
+	public void setKind(StockKind kind) {
+		this.kind = kind;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	public Stock() {
 	}
@@ -21,6 +56,20 @@ public class Stock {
 	
 	public void printInfo() {
 		System.out.println("id:"+id+" item:"+item+" name:"+name);
+	}
+	
+	public void getUserInput(Scanner input) {
+		System.out.print("Stock ID: "); 
+	    int id=input.nextInt();
+	    this.setId(id);
+	    
+		System.out.print("Stock Item: ");
+		String item=input.next();
+		this.setItem(item);
+		
+		System.out.print("stock Name: ");
+		String name=input.next();
+		this.setName(name);
 	}
 
 }
